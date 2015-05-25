7da956e6b2d9740fb48bbdefcdd931b1575676cf
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-05-25 08:27:12 -0700

    Making App class smaller

--- src/main/java/org/kohsuke/xstich/App.java
+++ src/main/java/org/kohsuke/xstich/App.java
@@ -19,49 +19,6 @@ import java.util.Map;
  *
  */
 public class App {
-    private static class Use implements Comparable<Use> {
-        /**
-         * Number of pixesl in this color.
-         */
-        int pixels;
-        /**
-         * Entry in the color palette.
-         */
-        final Entry color;
-        /**
-         * Symbol we use in the schematics.
-         */
-        final char letter;
-        /**
-         * Unique sequence number assigned to {@link Use}s
-         */
-        final int index;
-
-        private Use(Entry color, int index) {
-            this.color = color;
-            this.letter = SYMBOLS.charAt(index);
-            this.index = index;
-        }
-
-        /**
-         * Sort by the dominant color first.
-         */
-        public int compareTo(Use that) {
-            return that.pixels-this.pixels;
-        }
-        
-        public String toRGB() {
-            return String.format("rgb(%d,%d,%d)",color.rgb.getRed(), color.rgb.getGreen(), color.rgb.getBlue());
-        }
-
-        private static final String SYMBOLS = "＋Ｅー＊＃・＜□◇＝｜×ＺＮ∥％●○◎／ABX￥";
-
-        public void formatCellStyle(StringBuilder o) {
-            o.append(String.format("#schematic.color TD.c%d { background-color:%s; }\n", index, toRGB()));
-            o.append(String.format("#schematic.c%d TD.c%d { color:white; background-color: red; }\n", index, index));
-        }
-    }
-    
     public static void main(String[] args) throws Exception {
         ColorPalette dmc = new ColorPalette("dmc-floss");
         Map<Entry,Use> used = new LinkedHashMap<Entry,Use>();
---
+++ src/main/java/org/kohsuke/xstich/Use.java
@@ -0,0 +1,51 @@
+package org.kohsuke.xstich;
+
+import org.kohsuke.xstich.ColorPalette.Entry;
+
+/**
+ * Use of a palette entry in a specific image.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+class Use implements Comparable<Use> {
+    /**
+     * Number of pixesl in this color.
+     */
+    int pixels;
+    /**
+     * Entry in the color palette.
+     */
+    final Entry color;
+    /**
+     * Symbol we use in the schematics.
+     */
+    final char letter;
+    /**
+     * Unique sequence number assigned to {@link Use}s
+     */
+    final int index;
+
+    Use(Entry color, int index) {
+        this.color = color;
+        this.letter = SYMBOLS.charAt(index);
+        this.index = index;
+    }
+
+    /**
+     * Sort by the dominant color first.
+     */
+    public int compareTo(Use that) {
+        return that.pixels-this.pixels;
+    }
+
+    public String toRGB() {
+        return String.format("rgb(%d,%d,%d)",color.rgb.getRed(), color.rgb.getGreen(), color.rgb.getBlue());
+    }
+
+    private static final String SYMBOLS = "＋Ｅー＊＃・＜□◇＝｜×ＺＮ∥％●○◎／ABX￥";
+
+    public void formatCellStyle(StringBuilder o) {
+        o.append(String.format("#schematic.color TD.c%d { background-color:%s; }\n", index, toRGB()));
+        o.append(String.format("#schematic.c%d TD.c%d { color:white; background-color: red; }\n", index, index));
+    }
+}
